0cf1e37f3b523e24dd1e60632a269618d5049b29
姚建忠 <hunt-man@163.com>
2024-06-20 19:56:05 +0800

    增加qim8658陀螺仪驱动

--- main/ui/emoil_screen.py
+++ main/ui/emoil_screen.py
@@ -1,14 +1,17 @@
+import time
+
 from ui.screen import Screen
 import lvgl as lv
 from ui.emoil import Emoil
 import random
 from lib.qmi_8658 import QMI8658
 
+
 class EmoilScreen(Screen):
     def __init__(self):
         super().__init__()
-        self.qmi8658 = None;
-        self.checkmiuTimer = None;
+        self.qmi8658 = None
+        self.checkmiuTimer = None
         self.SetFlag(self.screen, lv.obj.FLAG.CLICKABLE, False)
         self.SetFlag(self.screen, lv.obj.FLAG.PRESS_LOCK, False)
         self.SetFlag(self.screen, lv.obj.FLAG.SCROLLABLE, False)
@@ -39,17 +42,22 @@ class EmoilScreen(Screen):
         self.ui_EmoilScreen_Container1.set_scrollbar_mode(lv.SCROLLBAR_MODE.OFF)
         self.ui_EmoilScreen_Container1.set_style_bg_color(lv.color_hex(0xFF), lv.PART.MAIN | lv.STATE.DEFAULT)
         print("init1")
-        self.total_emoil=10
-        self.current_emoil=1
-        self.last_emoil=Emoil(self.ui_EmoilScreen_Container1, f'ui/assets/e{self.current_emoil:02}.json')
+
+        self.turn_num = 0
+        self.turn_num2 = 0
+        self.turn_time = time.ticks_ms()
+        self.turn_time2 = time.ticks_ms()
+        self.total_emoil = 10
+        self.current_emoil = 1
+        self.last_emoil = Emoil(self.ui_EmoilScreen_Container1, f'ui/assets/e{self.current_emoil:02}.json')
         self.screen.add_event_cb(self.EmoilScreen_eventhandler, lv.EVENT.ALL, None)
         print("init2")
-        lv.timer_create(self.timer_callback,30000,None);
+        lv.timer_create(self.timer_callback, 30000, None);
 
-    def EmoilScreen_eventhandler(self,event_struct):
+    def EmoilScreen_eventhandler(self, event_struct):
         event = event_struct.code
         if event == lv.EVENT.GESTURE:
-            direction=lv.indev_t.get_gesture_dir(lv.indev_get_act())
+            direction = lv.indev_t.get_gesture_dir(lv.indev_get_act())
             if direction & lv.DIR.LEFT == lv.DIR.LEFT:
                 print("左滑")
             if direction & lv.DIR.RIGHT == lv.DIR.RIGHT:
@@ -59,23 +67,43 @@ class EmoilScreen(Screen):
             self.top_Animation(self.ui_EmoilScreen_Image1, 0)
         if event == lv.EVENT.SCREEN_LOADED:
             self.qmi8658 = QMI8658()
-            self.checkmiuTimer = lv.timer_create(self.timer_callback, 500, None);
+            self.checkmiuTimer = lv.timer_create(self.timer_callback, 200, None);
         if event == lv.EVENT.SCREEN_UNLOADED:
             lv.timer_del(self.checkmiuTimer)
             self.qmi8658.Close()
         return
 
-    def timer_callback(self,timer):
+    def timer_callback(self, timer):
         _, _, _, gyro_x, gyro_y, gyro_z = self.qmi8658.Read_Raw_XYZ()
-        if abs(gyro_z) > 25000:
-            if gyro_z > 0:
-                print("右转")
-            else:
-                print("左转")
-            last_num = self.current_emoil
-            while self.current_emoil == last_num:
-                self.current_emoil = random.randint(1, self.total_emoil)
-            Emoil(self.ui_EmoilScreen_Container1, f'ui/assets/e{self.current_emoil:02}.json')
+        if abs(gyro_z) > 10000:
+            # if gyro_z > 0:
+            #     print("右转")
+            # else:
+            #     print("左转")
+            old_emoil = self.current_emoil
+
+            self.turn_num += 1
+            self.turn_num2 += 1
+
+            diff2 = time.ticks_diff(time.ticks_ms(), self.turn_time2)
+            if diff2 >= 10000:  # 持续10秒内转动6次，固定播放炫晕表情
+                if self.turn_num2 >= 6:
+                    print("固定表情")
+                    self.current_emoil = 5
+                self.turn_num2 = 0
+                self.turn_time2 = time.ticks_ms()
+            diff = time.ticks_diff(time.ticks_ms(), self.turn_time)
+            if diff >= 3000:
+                if self.turn_num >= 3:
+                    print("随机表情")
+                    last_num = self.current_emoil
+                    while self.current_emoil == last_num:
+                        self.current_emoil = random.randint(1, self.total_emoil)
+                self.turn_num = 0
+                self.turn_time = time.ticks_ms()
+
+            if old_emoil != self.current_emoil:
+                Emoil(self.ui_EmoilScreen_Container1, f'ui/assets/e{self.current_emoil:02}.json')
         if abs(gyro_y) > 25000:
             if gyro_y > 0:
                 print("前滚")
