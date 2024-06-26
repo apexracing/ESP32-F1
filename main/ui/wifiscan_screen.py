0714eed3e6e57aaf71dadcf6e364065695257462
顺水快递 <69035246+huntsman123@users.noreply.github.com>
2024-06-26 23:21:06 +0800

    修改wifi BUG

--- main/ui/wifiscan_screen.py
+++ main/ui/wifiscan_screen.py
@@ -97,7 +97,7 @@ class WiFiScanScreen(Screen):
         self.wifi_msg = msg
         self.wifi_conn_flag = flag
 
-    async def shutdown(self, timer):
+    async def shutdown(self):
         asyncio.sleep_ms(5000)
         self.app.shutdown()
         machine.reset()
